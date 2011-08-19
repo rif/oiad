98566b8756a09b2f9a36c5b5e652aa25950e3eb8
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-08-19 16:08:13 +0000

    one more

--- kohana/application/classes/factory/scrappers/IndieBike.php
+++ kohana/application/classes/factory/scrappers/IndieBike.php
@@ -6,11 +6,11 @@ PolyFactory::addScrapper("http://www.indiebike.com/The-Peloton/Deal-of-the-Day",
 class IndieBike extends AbstractScrapper {
 
 	 protected function _fillDetails($deal, $host){
-        	$deal->desc_short = $this->_xpath("");
-        	$deal->price = $this->_xpath("");
+        	$deal->desc_short = $this->_xpath("//h1[@style='font-size:medium']");
+        	$deal->price = $this->_xpath("//font[@size='3']");
         	$deal->desc_long = $this->_xpath("");
-        	$deal->pictures = $this->_xpath("");
-        	$deal->shipping = $this->_xpath("");
+        	$deal->pictures = $this->_get_host($host).$this->_xpath("//tr[@id='handle_itemMainPortlet']//img[@border='0']/@src");
+        	$deal->shipping = $this->_get_host($host).$this->_xpath("//div[@id='div__footer']/div/div[1]/div[2]/a[6]/@href");
     }
 }
 
