0c44ad1a43f902fdcac13938b8a924612690f358
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-08-19 16:09:41 +0000

    more scrappers

--- kohana/application/classes/factory/scrappers/Locobuy.php
+++ kohana/application/classes/factory/scrappers/Locobuy.php
@@ -6,11 +6,11 @@ PolyFactory::addScrapper("http://www.locobuy.com/", new Locobuy());
 class Locobuy extends AbstractScrapper {
 
 	 protected function _fillDetails($deal, $host){
-        	$deal->desc_short = $this->_xpath("");
-        	$deal->price = $this->_xpath("");
+        	$deal->desc_short = $this->_xpath("//div[@class='prodinfo']/h2");
+        	$deal->price = $this->_xpath("//div[@class='prodinfo']/h3");
         	$deal->desc_long = $this->_xpath("");
-        	$deal->pictures = $this->_xpath("");
-        	$deal->shipping = $this->_xpath("");
+        	$deal->pictures = $this->_xpath("//a[contains(@class,'productimg')]/img/@src");
+        	$deal->shipping = $this->_xpath("//div[@class='prodinfo']/h4");
     }
 }
 
--- kohana/application/classes/factory/scrappers/MamaBargains.php
+++ kohana/application/classes/factory/scrappers/MamaBargains.php
@@ -6,10 +6,10 @@ PolyFactory::addScrapper("http://www.mamabargains.com/", new MamaBargains());
 class MamaBargains extends AbstractScrapper {
 
 	 protected function _fillDetails($deal, $host){
-        	$deal->desc_short = $this->_xpath("");
-        	$deal->price = $this->_xpath("");
-        	$deal->desc_long = $this->_xpath("");
-        	$deal->pictures = $this->_xpath("");
+        	$deal->desc_short = $this->_xpath("//div[@class='headline']");
+        	$deal->price = $this->_xpath("//span[@class='price']");
+        	$deal->desc_long = $this->_xpath("//div[@class='left']");
+        	$deal->pictures = $this->_xpath("//img[@id='productShot']/@src");
         	$deal->shipping = $this->_xpath("");
     }
 }
