c97f7aee1ff3091d61692d01d9a2f2e9e0c837ca
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-09-04 11:27:05 +0000

    editing categories

---
+++ oiadapp/views/categories/edit.php
@@ -0,0 +1,31 @@
+<?php
+echo Form::open('/categories/edit/'.$cat->id);
+echo "<div>".Form::label('name', 'Category name:'); echo Form::input('name', $cat->name)."</div>";
+echo "<div>".Form::submit('submit', 'Submit')."</div>";
+echo Form::close();
+
+echo HTML::anchor('/cat/delete/'.$cat->id, 'Delete', array('id'=>'delete-link'));
+?>
+<br/>
+<br/>
+<br/>
+<h2>Sites in this category</h2>
+
+<ol>
+<?php
+    $sites = $cat->sites->find_all();
+    foreach($sites as $site){
+        echo "<li>".$site->name."</li>";
+    }
+?>
+</ol>
+
+<script type="text/javascript">
+	$('#delete-link').click(function(e){
+		if (confirm("Are you shure you want to delete this site?")) { 
+ 			$.get($(this).attr('href'));
+ 			window.location = '/categories/';
+		}
+		e.preventDefault();
+	});
+</script>
