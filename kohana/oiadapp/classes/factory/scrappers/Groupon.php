759d46f7476cb74306cc9dd4246fb8de05b4b89d
Radu Ioan Fericean <radu@fericean.ro>
2011-09-15 16:01:35 +0000

    started groupon scrapper

---
+++ kohana/oiadapp/classes/factory/scrappers/Groupon.php
@@ -0,0 +1,18 @@
+<?php
+defined('SYSPATH') OR die('No Direct Script Access');
+
+PolyFactory::addScrapper("http://www.groupon.com/", new Groupuon());
+
+class Groupuon extends AbstractScrapper {
+
+	 protected function _fillDetails($deal, $host){
+        	$deal->desc_short = $this->_xpath("");
+        	$deal->price = $this->_xpath("");
+        	$deal->desc_long = $this->_xpath("");
+        	$deal->pictures = $this->_xpath("");
+        	$deal->shipping = $this->_xpath("");
+    }
+}
+
+?>
+
