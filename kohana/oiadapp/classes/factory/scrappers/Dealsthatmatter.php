58ed9cbd6a455c4434c5e9f297c1ed54119a8d02
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-09-19 15:03:56 +0000

    more

---
+++ kohana/oiadapp/classes/factory/scrappers/Dealsthatmatter.php
@@ -0,0 +1,18 @@
+<?php
+defined('SYSPATH') OR die('No Direct Script Access');
+
+PolyFactory::addScrapper("http://www.dealsthatmatter.com/", new Dealsthatmatter());
+
+class Dealsthatmatter extends AbstractScrapper {
+  protected function _fillDetails($deal, $host){
+    $deal->item_link = "http://www.dealsthatmatter.com".$this->_xpath("//div[@id='buy_button']/a/@href");
+    $deal->desc_short = $this->_xpath("//div[@class='content_left'][1]/div[@class='tk-museo'][1]");
+    $deal->price = $this->_xpath("//div[@id='buy_button']");
+    $deal->desc_long = $this->_xpath("//div[@id='deal_body']/div[2]/div[2]/div[2]");
+    $deal->pictures = $this->_xpath("//div[@id='deal_body']//img/@src");
+    $deal->shipping = $this->_xpath("");
+  }
+}
+
+?>
+
