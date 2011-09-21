e395092f7a30e72c41ca3a9fc67e0a59ab8c4a9f
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-09-21 21:50:24 +0000

    added mamapedia

--- kohana/oiadapp/classes/factory/scrappers/Groopdealz.php
+++ kohana/oiadapp/classes/factory/scrappers/Groopdealz.php
@@ -1,7 +1,7 @@
 <?php
 defined('SYSPATH') OR die('No Direct Script Access');
 
-PolyFactory::addScrapper("http://www.thedealmap.com/", new Groopdealz());
+PolyFactory::addScrapper("http://www.groopdealz.com/", new Groopdealz());
 
 class Groopdealz extends AbstractMultipleScrapper {
     protected function _getDealIterator() {
@@ -9,12 +9,12 @@ class Groopdealz extends AbstractMultipleScrapper {
   }
 
   protected function _fillMultipleDetails($deal, $page, $count){
-    $deal->item_link = $this->_xpath("//a[@class='deal-title']/@href", $count);
+    $deal->item_link = "http://www.groopdealz.com".$this->_xpath("//div[@class='side_deals']//div[@class='view_deal_link']/a/@href", $count);
     $deal->desc_short = $this->_xpath("//div[@class='hp_side_deal_title']", $count);
-    $deal->price = $this->_xpath("//a[@class='deal-title']", $count);
-    $deal->desc_long = $this->_xpath("//div[@class='listing-address']", $count);
+    $deal->price = $this->_xpath("");
+    $deal->desc_long = $this->_xpath("");
     $deal->pictures = $this->_xpath("//div[@class='side_deals']//img/@src", $count);
-    $deal->shipping = $this->_xpath("", $count);
+    $deal->shipping = $this->_xpath("");
   }
 }
 
