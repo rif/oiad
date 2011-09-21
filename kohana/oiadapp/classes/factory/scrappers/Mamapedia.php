5db05987bf70415eeb46aac3a8c0bb4521e6f031
Radu Ioan Fericean <radu@fericean.ro>
2011-09-21 21:50:44 +0000

    Savvyfamilies

--- kohana/oiadapp/classes/factory/scrappers/Mamapedia.php
+++ kohana/oiadapp/classes/factory/scrappers/Mamapedia.php
@@ -11,8 +11,8 @@ class Mamapedia extends AbstractMultipleScrapper {
   protected function _fillMultipleDetails($deal, $page, $count){
     $deal->item_link = "http://deals.mamapedia.com".$this->_xpath("//a[@class='link_overlay']/@href", $count);
     $deal->desc_short = $this->_xpath("//div[@class='short_headline']", $count)." ".$this->_xpath("//div[@class='headline']", $count);
-    $deal->price = $this->_xpath("//li[@id='deal_price']");
-    $deal->desc_long = $this->_xpath("//div[@class='see_more_snippet']");
+    $deal->price = $this->_xpath("//li[@id='deal_price']", $count);
+    $deal->desc_long = $this->_xpath("//div[@class='see_more_snippet']", $count);
     $deal->pictures = $this->_xpath("//img[@class='deal_photo size388']/@src", $count);
     $deal->shipping = $this->_xpath("");
   }
