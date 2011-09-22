f39a4f2369915081a17225068c55f070621d39e8
Radu Ioan Fericean <radu@fericean.ro>
2011-09-22 09:07:53 +0000

    code cleanups

--- kohana/oiadapp/classes/factory/AbstractMultipleScrapper.php
+++ kohana/oiadapp/classes/factory/AbstractMultipleScrapper.php
@@ -4,52 +4,25 @@ defined('SYSPATH') OR die('No Direct Script Access');
 
 abstract class AbstractMultipleScrapper extends AbstractScrapper {
 
-    public function scrapp($site) {
+    protected function loadDeal($site){
       $today = date('Y-m-d');
-      $oldSetting = libxml_use_internal_errors(true);
-      libxml_clear_errors();
-      $context = NULL;
-      if ($site->cookie){
-        $opts = array('http' => array('header'=> 'Cookie: '.$site->cookie."\r\n"));
-        $context = stream_context_create($opts);
-      }
-      $contents = file_get_contents($site->page, false, $context);
-      try {
-            $html = new DOMDocument();
-            $html->loadHtml($contents);
-            $this->xpath = new DOMXPath($html);
-      } catch(ErrorException $e){
-        return $e->getMessage();
-      }
       $result = array();
-      $today = date('Y-m-d');
-      $prods = $this->_mxpath($this->_getDealIterator());
-      foreach($prods as $count=>$prod){
+      $items = $this->xpath->query($this->_getDealIterator());
+      for($i = 0; $i < $items->length; $i++){
         $deal = ORM::factory('deal');
-        $deal->where('site', '=', $site->id)->where('pub_date', '=', $today)->where('count', '=', $count)->find();
+        $deal->where('site', '=', $site->id)->where('pub_date', '=', $today)->where('count', '=', $i)->find();
         $deal->site = $site;
-        $this->_fillMultipleDetails($deal, $site->page, $count);
-        $deal->count = $count;
+        $this->_fillMultipleDetails($deal, $site->page, $i);
+        $deal->count = $i;
         $deal->pub_date = $today;
         $deal->save();
         array_push($result, $deal->id);
       }
-      libxml_clear_errors();
-      libxml_use_internal_errors($oldSetting);
       return $result;
     }
 
     protected function _getDealIterator() {}
     protected function _fillMultipleDetails($deal, $page, $count) {}
-
-    protected function _mxpath($expr) {
-      $items = $this->xpath->query($expr);
-      $result = array();
-      foreach($items as $item){
-        array_push($result, trim($item->nodeValue));
-      }
-      return $result;
-    }
 }
 
 ?>
