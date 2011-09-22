a2722399846e89c8e35a847524a5b32c3a58c3c1
Radu Ioan Fericean <radu@fericean.ro>
2011-09-22 20:19:16 +0000

    two sites more

--- kohana/oiadapp/classes/factory/scrappers/Meijer.php
+++ kohana/oiadapp/classes/factory/scrappers/Meijer.php
@@ -6,10 +6,10 @@ PolyFactory::addScrapper("http://www3.meijer.com/bigsteals.xml", new Meijer());
 class Meijer extends AbstractFeedScrapper {
   protected function _fillFeedDetails($deal, $item){
     $deal->item_link = $item['link'];
-    $deal->desc_short = $item['merchant_name'];
-    $deal->price = $item['price'];
-    $deal->desc_long = $item['description'].$item['details'];
-    $deal->pictures = $item['image_url'];
+    $deal->desc_short = $item['title'];
+    $deal->price = '';
+    $deal->desc_long = $item['description'];
+    $deal->pictures = '';
     $deal->shipping = '';
   }
 }
