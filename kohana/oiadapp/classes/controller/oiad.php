2eb390724275dc2fc2ca13fcc90921f6b67a2b50
Radu Ioan Fericean <radu@fericean.ro>
2011-09-27 15:50:23 +0000

    frontpage deals

--- kohana/oiadapp/classes/controller/oiad.php
+++ kohana/oiadapp/classes/controller/oiad.php
@@ -7,6 +7,8 @@ class Controller_Oiad extends Controller_Template {
  public function action_index() {
         $view = View::factory('oiad/list');
         
+        $view->categories = ORM::factory('category')->find_all();
+
         $deals = ORM::factory('deal');
         $today = date('Y-m-d');
         $view->deals = $deals->where('pub_date', '=', $today)->find_all();
--- kohana/oiadapp/views/oiad/list.php
+++ kohana/oiadapp/views/oiad/list.php
@@ -1,12 +1,16 @@
-<ol>
+<div id="oiad-deals">
 <?php
-$i = 0;
+foreach($categories as $cat){
+    echo '<a class="category-link selected" href="#">'.$cat->name.'</a>&nbsp;';
+}
+
 foreach($deals as $d)
 {
     $site = ORM::factory('site',$d->site);
-    echo '<li>';
-    echo HTML::anchor('/deals/showdeal/'.$d->id, $site->name)."<br/>";
-    echo "<b>Site link: </b>".HTML::anchor($site->page, $site->page)."<br/>"; 
+    $site_categories = $site->categories->find_all();
+    echo '<div class="';foreach($site_categories as $c){echo $c->name.' ';}echo '">';
+    echo HTML::anchor("#",$d->desc_short, array('class'=>'deal-title'));
+    echo '<div class="deal-body">';
     echo "<b>Item link: </b>".HTML::anchor($d->item_link, $d->item_link)."<br/>"; 
     echo "<b>Pictures</b>:<br/>";
     if($d->pictures){
@@ -15,12 +19,25 @@ foreach($deals as $d)
             echo '<img src="'.$p.'" alt="'.$p.'"/>';
         }
         echo "</ul>";
-    }
-    echo "<b>Desc short</b>:".$d->desc_short."<br/>";
+    }    
     echo "<b>Desc long</b>:".$d->desc_long."<br/>";
     echo "<b>Price</b>:".$d->price."<br/>";
     echo "<b>Shipping</b>:".$d->shipping."<br/>";
-    echo "</li>";
+    echo '</div>';
+    echo "</div>";
 }
 ?>
-</ol>
+</div>
+<script type="text/javascript">
+    $(function(){
+        $(".category-link").click(function(e){
+            $(this).toggleClass('selected');
+            $("div[class*='" + $(this).text() + "']").fadeToggle();
+            e.preventDefault();
+        });
+        $(".deal-title").click(function(e){
+            $(this).next(".deal-body").slideToggle();
+            e.preventDefault();
+        });
+    });
+</script>
