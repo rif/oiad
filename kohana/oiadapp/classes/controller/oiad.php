93765650890c80b2c7c04dc96f2ec3d392fd7d62
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-10-22 16:15:24 +0000

    uniform, constants

--- kohana/oiadapp/classes/controller/oiad.php
+++ kohana/oiadapp/classes/controller/oiad.php
@@ -6,10 +6,20 @@ class Controller_Oiad extends Controller_Template {
 
     public function action_index() {
         $view = View::factory('oiad/list');
-        
-        $view->categories = ORM::factory('category')->find_all();
+         /*$pagination = new Pagination(array(
+			'total_items' => $total, 
+			'items_per_page' => 9,  // set this to 30 or 15 for the real thing, now just for testing purposes...
+			'auto_hide' => false,			
+		));
+		$result = $user->limit($pagination->items_per_page)
+			->offset($pagination->offset)
+			->order_by($sort, $dir)
+			->find_all();*/
         $view->sites = ORM::factory('site')->where('is_deal','=','F')->find_all();
-        
+       	/*$this->template->content = View::factory('user/admin/index')
+			->set('users', $result)
+			->set('paging', $pagination)
+			->set('default_sort', $sort);*/
         $this->template->content = $view;
     }
     
