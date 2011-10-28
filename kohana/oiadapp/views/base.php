db3f9973f9294963ba9c5925752285a914bbd640
Radu Ioan Fericean <radu@fericean.ro>
2011-10-28 17:19:15 +0000

    remove jquery ui

--- kohana/oiadapp/views/base.php
+++ kohana/oiadapp/views/base.php
@@ -14,10 +14,9 @@
         <?php echo HTML::style('media/stylesheets/screen.css', array('media'=>'screen, projection'));?>                
   		<?php echo HTML::style('media/stylesheets/print.css', array('media'=>'print'));?>
         <!--[if lt IE 8]><?php echo HTML::style('media/stylesheets/ie.css', array('media'=>'screen, projection'));?><![endif]-->
-        <?php echo HTML::style('media/stylesheets/flick/jquery-ui-1.8.16.custom.css', array('media'=>'screen, projection'));?>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
         <script>!window.jQuery && document.write('<script src="/media/js/jquery-1.6.4.min.js" type="text/javascript"><\/script>')</script>        
-        <?php echo HTML::script('media/js/jquery-ui-1.8.16.custom.min.js');?>               
+            
     </head>
     
     <body>
