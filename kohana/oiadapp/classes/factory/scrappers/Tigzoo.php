9113b069ef0f90e5b98811ac8d2dc0440d86dce0
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-11-04 19:18:20 +0000

    style changes

--- kohana/oiadapp/classes/factory/scrappers/Tigzoo.php
+++ kohana/oiadapp/classes/factory/scrappers/Tigzoo.php
@@ -8,9 +8,9 @@ class Tigzoo extends AbstractScrapper {
 	 protected function _fillDetails($deal, $host){
         	$deal->desc_short = $this->_xpath("//h2[@class='title']/a");
         	$deal->price = $this->_xpath("//h1/strong/span");
-        	$deal->desc_long = $this->_xpath("//div[@class='post-content clear-block']/p[3]");
-        	$deal->pictures = $this->_xpath("//img[@width='250']/@src");
-        	$deal->shipping = $this->_xpath("//div[@class='post-content clear-block']/p[2]");
+        	$deal->desc_long = $this->_xpath("//div[@class='post-content clear-block']//ul");
+        	$deal->pictures = $this->_xpath("//img[@class='alignleft size-full wp-image-3988']/@src");
+        	$deal->shipping = $this->_xpath("//div[contains(@class,'post-content clear-block')]/p[3]");
     }
 }
 
--- kohana/oiadapp/classes/factory/scrappers/TotallyAnything.php
+++ kohana/oiadapp/classes/factory/scrappers/TotallyAnything.php
@@ -7,8 +7,8 @@ class TotallyAnything extends AbstractScrapper {
 
 	 protected function _fillDetails($deal, $host){
         	$deal->desc_short = $this->_xpath("//h1[@class='dod_product_name_H1']");
-        	$deal->price = $this->_xpath("//span[@class='dod_product_sale_price']");
-        	$deal->desc_long = $this->_xpath("//span[@class='cat_text_list']/span");
+        	$deal->price = $this->_xpath("//span[contains(.,'$')][@class='dod_product_sale_price']");
+        	$deal->desc_long = $this->_xpath("//div[@id='desc_div']/p");
         	$deal->pictures = $host.$this->_xpath("//img[@id='img_main']/@src");
         	$deal->shipping = $this->_xpath("//p[contains(.,'Shipping')]");
     }
