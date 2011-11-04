414790111b96449093b00256552883302da9744d
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-11-04 15:47:54 +0000

    more scrappers and coupon separation

--- kohana/oiadapp/classes/factory/scrappers/RedTagSports.php
+++ kohana/oiadapp/classes/factory/scrappers/RedTagSports.php
@@ -6,11 +6,11 @@ PolyFactory::addScrapper("http://www.redtagsports.com/", new RedTagSports());
 class RedTagSports extends AbstractScrapper {
 
 	 protected function _fillDetails($deal, $host){
-        	$deal->desc_short = $this->_xpath("//div[@id='rightcolumn']/h1");
-        	$deal->price = $this->_xpath("//h1[@id='price']");
-        	$deal->desc_long = $this->_xpath("//div[@id='contenthold']");
-        	$deal->pictures = $this->_xpath("//img[@name='targetimage']/@src");
-        	$deal->shipping = $this->_xpath("//div[@id='shipping']/a/@href");
+        	$deal->desc_short = $this->_xpath("//div[@class='content_right']/h3");
+        	$deal->price = $this->_xpath("//span[@class='rate']");
+        	$deal->desc_long = $this->_xpath("//div[@id='content']");
+        	$deal->pictures = $this->_xpath("//div[@class='content_left']/a/img/@src");
+        	$deal->shipping = $this->_xpath("//div[@class='textwidget']/p/a/@href");
     }
 }
 
