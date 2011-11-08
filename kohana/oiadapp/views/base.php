e6d1eb6db2e16fd1b47e9ab82655932b906a1c84
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-11-08 16:23:29 +0000

    new jquery version

--- kohana/oiadapp/views/base.php
+++ kohana/oiadapp/views/base.php
@@ -16,8 +16,8 @@
   		<?php echo HTML::style('media/stylesheets/print.css', array('media'=>'print'));?>
         <!--[if lt IE 8]><?php echo HTML::style('media/stylesheets/ie.css', array('media'=>'screen, projection'));?><![endif]-->
         <?php echo HTML::style('media/stylesheets/chosen.css', array('media'=>'screen, projection'));?>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-        <script>!window.jQuery && document.write('<script src="/media/js/jquery-1.6.4.min.js" type="text/javascript"><\/script>')</script>        
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+        <script>!window.jQuery && document.write('<script src="/media/js/jquery-1.7.min.js" type="text/javascript"><\/script>')</script>        
         <?php echo HTML::script('media/js/chosen.jquery.min.js');?>           
     </head>
     
