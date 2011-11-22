22db48021d2a2d7165aa63cf6dba605ef8d802ae
ldandy <ldandy@localhost>
2011-11-22 23:32:33 +0200

    added the report for out of date scrapers

--- kohana/oiadapp/views/reports/categories.php
+++ kohana/oiadapp/views/reports/categories.php
@@ -5,27 +5,25 @@
 	<tbody>
 	<?php
 	
-	echo '<tr class="even">';
-	echo '<td class="large">'.HTML::anchor('/reports/sites/', __('All sites')).'</td>';
-	echo '</tr>';
-
-	echo '<tr class="odd">';
-	echo '<td class="large">'.HTML::anchor('/reports/categoryless/', __('Sites not assigned to a category')).'</td>';
-	echo '</tr>';
-	
-	echo '<tr class="even">';
-	echo '<td class="large">'.HTML::anchor('/reports/olddeals/?days=2', __('Sites not updated in more than 48 hours')).'</td>';
-	echo '</tr>';
-	
-	echo '<tr class="odd">';
-	echo '<td class="large">'.HTML::anchor('/reports/olddeals/?days=7', __('Sites not updated in more than a week')).'</td>';
-	echo '</tr>';
-	
-	echo '<tr class="even">';
-	echo '<td class="large">'.HTML::anchor('/reports/olddeals/?days=30', __('Sites not updated in more than 30 days')).'</td>';
-	echo '</tr>';	
+	$report_links = array(
+		array('url' => '/reports/sites/', 'text'=>'All sites'),
+		array('url' => '/reports/categoryless/', 'text'=>'Sites not assigned to a category'),
+		array('url' => '/reports/outofdate/', 'text'=>'Sites with out of date scrapers'),
+		array('url' => '/reports/olddeals/?days=2', 'text'=>'Sites not updated in more than 48 hours'),
+		array('url' => '/reports/olddeals/?days=7', 'text'=>'Sites not updated in more than a week'),
+		array('url' => '/reports/olddeals/?days=30', 'text'=>'Sites not updated in more than 30 days')
 		
-	$tr_classes = Array('odd','even');
+	);
+	
+	$tr_classes = Array('even','odd');
+	
+	foreach($report_links as $i=>$link)
+	{
+		echo '<tr class="'.$tr_classes[$i%2].'">';
+		echo '<td class="large">'.HTML::anchor($link["url"], __($link["text"])).'</td>';
+		echo '</tr>';
+	}
+		
 	foreach($categories as $i=>$cat)
 	{
 		echo '<tr class="'.$tr_classes[$i%2].'">';
