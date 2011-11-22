caddf10f20505976e6a7eeb7b824bec45ba2519b
radu fericean <radu.fericean@5d56e146-3ffd-0310-b524-d243ab4a3b00>
2011-11-22 18:55:54 +0000

    nono more site auto deactivation and a scraper fixed

--- kohana/oiadapp/classes/factory/scrapers/DailydealsNet.php
+++ kohana/oiadapp/classes/factory/scrapers/DailydealsNet.php
@@ -9,7 +9,7 @@ class DailydealsNet extends AbstractScraper {
         	$deal->desc_short = $this->_xpath("//div[@id='productNameWrap']");
         	$deal->price = $this->_xpath("//div[@id='dd-1653']/div[@id='productPageWrap']/div[@id='productInfoWrap']/div[@id='productPriceWrap']/div[@id='variantPrice']/div[@id='price']");
         	$deal->desc_long = $this->_xpath("//div[@id='productDescWrap']");
-        	$deal->pictures = "http://www.dealgenius.com/".$this->_xpath("//div[@id='dd-1653']//img[contains(@src,'Product')]/@src");
+        	$deal->pictures = "http://www.dealgenius.com/".$this->_xpath("//div[@id='mainpic']/img/@src");
         	$deal->shipping = $this->_xpath("//span[@class='freeShippingWrap']");
     }
 }
